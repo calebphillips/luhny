82d6c6926d16b38ded372b697bc8b23948fe854f
calebphillips <caleb.phillips@gmail.com>
2012-03-23 15:40:05 -0500

    Add a little clean up and the partitioning fn

--- src/luhny/core.clj
+++ src/luhny/core.clj
@@ -1,14 +1,12 @@
 (ns luhny.core)
 
 (defn str->ints [s]
-  (map #(Integer/parseInt (str %))
-       (seq s)))
+  (map #(Character/digit % 10) s))
 
 (defn double-every-second [s]
-  (reverse
-   (map #(%1 %2)
-        (cycle  [identity #(* % 2)])
-        (reverse s))))
+  (map #(%1 %2)
+       (cycle  [identity #(* % 2)])
+       (reverse s)))
 
 (defn ints->digits [s]
   (mapcat #(str->ints (str %)) s))
@@ -25,5 +23,22 @@
       zero?))
 
 
-
-
+(defn partition-ignoring
+  "Acts like the 3 argument version of partition, except that it 'ignores'
+   the characters in the ignore set.  It includes them in the partiion,
+   but does not count them when building the partition.
+   The idea is to use this to divide a sequences of characters up into partitions
+   of n characters, not counting spaces and hyphens, but preserving them so that
+   we can spit them back out once we mask any cc numbers"
+  [n step ignore coll]
+  (let [cnt-ignoring #(count (remove ignore %))
+        full-partition? #(= n (cnt-ignoring %))
+        take-ignoring #(loop [c % taken []]
+                         (if (or (full-partition? taken) (empty? c))
+                           taken
+                           (recur (rest c) (conj taken (first c)))))]
+    (lazy-seq
+     (when-let [s (seq coll)]
+       (let [p (take-ignoring s)]
+         (when (full-partition? p)
+           (cons p (partition-ignoring n step ignore (nthnext s step)))))))))
--- test/luhny/test/core.clj
+++ test/luhny/test/core.clj
@@ -8,7 +8,7 @@
        [1 2 3 4 5 4 3 2 1] "123454321"))
 
 (deftest test-double-every-second
-  (are [x y] (= x (double-every-second y))
+  (are [x y] (= (reverse x) (double-every-second y))
        [1 4 3 8 5] [1 2 3 4 5]
        [10 6 14 8] [5 6 7 8]))
 
@@ -21,3 +21,16 @@
        "5678"
        "4563960122001999")
   (is (not (luhny? "6789"))))
+
+(deftest test-partition-ignoring
+  (is (= [[1 2 :w :x 3]
+          [2 :w :x 3 :y :x 4]
+          [:w :x 3 :y :x 4 5]
+          [:x 3 :y :x 4 5]
+          [3 :y :x 4 5]
+          [:y :x 4 5 :w 6]
+          [:x 4 5 :w 6]
+          [4 5 :w 6]
+          ]
+         (partition-ignoring 3 1 #{:w :x :y}
+                             [1 2 :w :x 3 :y :x 4 5 :w 6]))))
